e0d9223496dc921ef22dc9927aeaaffccb91c880
aservin6 <servin.alejandroii@gmail.com>
2022-12-22 22:48:48 -0600

    overflow on mobile fix attempt

--- src/components/ProductShowcase/CarouselButtons.jsx
+++ src/components/ProductShowcase/CarouselButtons.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const CarouselButtons = () => {
   const [selectedImage, setSelectedImage] = useState("1");
