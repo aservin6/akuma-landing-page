754fdbf71b225bc4e80ff0f69b96683569af47d1
aservin6 <servin.alejandroii@gmail.com>
2023-04-24 00:44:00 -0500

    gallery image one sizing fix attempt

--- src/components/Gallery/ImageThree.jsx
+++ src/components/Gallery/ImageThree.jsx
@@ -11,13 +11,18 @@ const ImageThree = () => {
 
   return (
     <div ref={ref} className="relative w-full pb-16 md:pb-0">
-      <img src={chromaWeight} className="border border-white rounded-md border-opacity-30 md:m-0" alt="chroma brass weight" />
-      <p className={`absolute text-2xl lg:text-3xl md:bottom-4 font-semibold translate-x-full transition-all duration-1000 ${
+      <img
+        src={chromaWeight}
+        className="border border-white rounded-md border-opacity-30 md:m-0"
+        alt="chroma brass weight"
+      />
+      <p
+        className={`absolute text-2xl mt-1 lg:text-3xl md:bottom-4 font-semibold translate-x-full transition-all duration-1000 ${
           inView ? "opacity-100 translate-x-0 md:translate-x-4" : "opacity-0"
-        }`}>
-            <span className="text-blue-300">Laser</span> etched brass
-            weight
-          </p>
+        }`}
+      >
+        <span className="text-blue-300">Laser</span> etched brass weight
+      </p>
     </div>
   );
 };
--- src/components/Gallery/ImageTwo.jsx
+++ src/components/Gallery/ImageTwo.jsx
@@ -11,12 +11,18 @@ const ImageTwo = () => {
 
   return (
     <div ref={ref} className="relative w-full pb-16 md:pb-0">
-      <img src={akumaBadging} className="border border-white rounded-md border-opacity-30 md:m-0" alt="keyboard akuma badging" />
-      <p className={`absolute z-10 text-2xl lg:text-3xl md:bottom-4 font-semibold translate-x-full transition-all duration-1000 ${
+      <img
+        src={akumaBadging}
+        className="border border-white rounded-md border-opacity-30 md:m-0"
+        alt="keyboard akuma badging"
+      />
+      <p
+        className={`absolute z-10 mt-1 text-2xl lg:text-3xl md:bottom-4 font-semibold translate-x-full transition-all duration-1000 ${
           inView ? "opacity-100 translate-x-0 md:translate-x-4" : "opacity-0"
-        }`}>
-            Exclusive <span className="text-rose-300">Akuma</span> badging
-          </p>
+        }`}
+      >
+        Exclusive <span className="text-rose-300">Akuma</span> badging
+      </p>
     </div>
   );
 };
